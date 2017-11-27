f462312203b2595a16f95e3a624229db539e272b
Jun Wang <junbill.wang@samsung.com>
2017-11-27 11:06:05 +0800

    Add disable-debuginfo option which disable to generate debuginfo packages
    Change-Id: Ic4ca1753bcadda8555daee96e13c2f7d23032859

--- data/gbs.sh
+++ data/gbs.sh
@@ -84,7 +84,7 @@ __gbs ()
         --arch= --repository= --dist= --buildroot= --clean
         --include-all --extra-packs= --spec= --commit= --cache
         --skip-conf-repos --profile= --noinit --keep-packs --use-higher-deps
-        --not-export-source --clean-repos --define --baselibs
+        --not-export-source --clean-repos --define --baselib --disable-debuginfo
     "
     cr_opts="
         --profile= --tmpfs --ks-file
